8547219db26d9f418492ada16d99e4385cc7a221
Guilherme Carvalho <guiiivs@hotmail.com>
2020-06-23 17:45:10 -0300

    Commit inicial

---
+++ js/script.js
@@ -0,0 +1,21 @@
+function Soma(){
+    var number=document.getElementById("txtnumero1").value;  
+    n1 = document.getElementById('txtnumero1').value;
+    n2 = document.getElementById('txtnumero2').value;
+    resultado = parseInt(n1) + parseInt(n2);
+    document.getElementById('txtresultado').value = resultado;
+
+    return
+}
+
+function Subtrai(){
+    return
+}
+
+function Multiplica(){
+    return
+}
+
+function Divide(){
+    return
+}
